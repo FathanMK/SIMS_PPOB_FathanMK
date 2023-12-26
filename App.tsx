39ff21c689c4da7f665da697ab280051162fd952
Fathan MK <emailfathanmk@gmail.com>
2023-12-26 18:35:50 +0700

    feat: Splashscreen

--- App.tsx
+++ App.tsx
@@ -1,5 +1,10 @@
+import {useEffect} from 'react';
 import Provider from './src/provider';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return <Provider />;
 }
